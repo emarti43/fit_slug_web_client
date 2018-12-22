b2d3b4ca2fb78e7e2977e7ead995e681f16a852d
bastion6429 <emarti43@ucsc.edu>
2018-12-21 20:14:57 -0800

    fully working with api now

--- containers/App.js
+++ containers/App.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
 import MealList from './MealList';
 import ExerciseList from './ExerciseList';
+import $ from 'jquery';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
     }
-
     componentDidMount() {
-
+        $.ajax({
+            url: 'http://127.0.0.1:3000/meals.json',
+            type: 'get',
+            success: function (response) {
+                // handle the response
+                console.log(response);
+            },
+            error: function (xhr, status) {
+                console.log('couldn\'t');
+            },
+            dataType: 'html'
+        });
     }
 
     componentWillUnmount() {
@@ -18,7 +29,7 @@ export default class App extends React.Component {
     render () {
         return <div>
         This is my new react app
-        <MealList list={["Chicken Tikki Masala", "Sticky Rice", "Lentils"]}/>
+        <MealList list={["Chicken Tikki Masala", "Sticky Rice"]}/>
         <ExerciseList list={["Bicep Curl", "Push Ups", "Bench Press"]}/>
         </div>
     }
