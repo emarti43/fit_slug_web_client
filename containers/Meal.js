6ba178f3d092d5a6a5ea0813230977a7b92e94c7
bastion6429 <emarti43@ucsc.edu>
2018-12-30 22:49:31 -0800

    showForm functionality working

--- containers/Meal.js
+++ containers/Meal.js
@@ -2,6 +2,13 @@ import React, {Component} from 'react';
 export default class Meal extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {showForm: false};
+        this.showForm = this.showForm.bind(this);
+    }
+
+    showForm(e) {
+      e.preventDefault();
+      this.setState({showForm: !this.state.showForm});
     }
 
     componentDidMount() {
@@ -12,22 +19,26 @@ export default class Meal extends React.Component {
 
     }
     render () {
-        return <div className = "card">
-        <h4>{this.props.mealData.name}</h4>
-        <ul>
-          <li> Amount Per {this.props.mealData.serving_size}</li>
-          <li> Calories {this.props.mealData.kcal} </li>
-          <li> Total Fat {this.props.mealData.total_fat} g</li>
-          <li> Saturated Fat {this.props.mealData.sat_fat}g</li>
-          <li> Monounsaturated Fat {this.props.mealData.monoun_fat} g</li>
-          <li> Cholesterol {this.props.mealData.cholesterol} g</li>
-          <li> Sodium {this.props.mealData.sodium} g</li>
-          <li> Potassium {this.props.mealData.potassium} g</li>
-          <li> Total Carbohydrate {this.props.mealData.total_carb} g</li>
-          <li> Dietary fiber {this.props.mealData.fiber} g</li>
-          <li> Sugar {this.props.mealData.sugar} g</li>
-          <li> Protein {this.props.mealData.protein} g</li>
-        </ul>
-        </div>
+      const form = <div>sample text</div>
+      const mealItem = <div className = "card">
+      <h4>{this.props.mealData.name}</h4>
+      <ul>
+        <li> <b>Amount Per</b> {this.props.mealData.serving_size}</li>
+        <li> <b>Calories</b> {this.props.mealData.kcal} </li>
+        <li> <b>Total Fat</b> {this.props.mealData.total_fat} g</li>
+        <li> <b>Saturated Fat</b> {this.props.mealData.sat_fat} g</li>
+        <li> <b>Monounsaturated Fat</b> {this.props.mealData.monoun_fat} g</li>
+        <li> <b>Cholesterol</b> {this.props.mealData.cholesterol} g</li>
+        <li> <b>Sodium</b> {this.props.mealData.sodium} g</li>
+        <li> <b>Potassium</b> {this.props.mealData.potassium} g</li>
+        <li> <b>Total Carbohydrate</b> {this.props.mealData.total_carb} g</li>
+        <li>    Dietary fiber {this.props.mealData.fiber} g</li>
+        <li>    Sugar {this.props.mealData.sugar} g</li>
+        <li> <b>Protein</b> {this.props.mealData.protein} g</li>
+      </ul>
+      <a className="waves-effect waves-light btn" onClick={this.showForm}>add meal</a>
+      {(this.state.showForm)? form: <div></div>}
+      </div>;
+      return mealItem;
     }
 }
