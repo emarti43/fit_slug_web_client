4711b96a42cf6be4fdfc8de90847fab5ba1b79e8
bastion6429 <emarti43@ucsc.edu>
2019-08-13 14:10:39 -0700

    axios request will have corresponding api roots for different envs

--- containers/utils/RequestTemplate.js
+++ containers/utils/RequestTemplate.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 const axios = require('axios');
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('fit_slug_session');
 
@@ -6,7 +5,7 @@ class RequestTemplate {
   static genericRequest(method, endpoint, data={}) {
     let config = {
       method: method,
-      url: 'http://localhost:3000/api/' + endpoint,
+      url: process.env.API_ROOT + endpoint,
       data: data,
     }
     return axios(config);
---
+++ webpack/config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var path = require('path');
+var parentDir = path.join(__dirname, '../')
+module.exports = {
+  mode: 'development',
+  entry: [
+    path.join(parentDir, 'index.js')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options:
+            {
+              presets: ['@babel/react']
+            }
+          }
+        ],
+      },
+      {
+        test: /\.less$/,
+        loaders: ["style-loader", "css-loader", "less-loader"]
+      }
+    ]
+  },
+  output: {
+    path: parentDir + '/dist',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: parentDir,
+    historyApiFallback: true
+  }
+}
