b38db3fc3abae321f249b7c6eb8686d92584669c
bastion6429 <emarti43@ucsc.edu>
2019-09-26 14:32:11 -0700

    placeholder for about page

--- containers/pages/About.js
+++ containers/pages/About.js
@@ -3,9 +3,21 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return (
-        <div>
-          <h2>About</h2>
+      <div>
+        <div className="parallax-container" style={{height: "300px"}}>
+          <div className="parallax">
+            <img style={{width: "100%"}} src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/master/pass/ba-tikka-masala-2.jpg"/>
+          </div>
         </div>
+        <div className="section white">
+          <div className="container">
+            <h3 className="light-blue-text">About</h3>
+            <p>
+              FitSlug is a personal project created for educational purposes.
+            </p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- index.js
+++ index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 // main app
 import App from './containers/App';
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+ }
+);
+
 ReactDOM.render(<App/>, document.getElementById('app'));
