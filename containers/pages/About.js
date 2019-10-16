2c9529ff1f0f1d547e9935d9a2d34508b84a792e
bastion6429 <emarti43@ucsc.edu>
2019-10-16 14:58:31 -0700

    updated about page

--- containers/pages/About.js
+++ containers/pages/About.js
@@ -6,14 +6,14 @@ class About extends Component {
       <div>
         <div className="parallax-container" style={{height: "300px"}}>
           <div className="parallax">
-            <img style={{width: "100%"}} src="https://assets.bonappetit.com/photos/5b69f163d3d14670539a2174/master/pass/ba-tikka-masala-2.jpg"/>
+            <img style={{width: "100%"}} src="/food.jpg"/>
           </div>
         </div>
         <div className="section white">
           <div className="container">
             <h3 className="light-blue-text">About</h3>
             <p>
-              FitSlug is a personal project created for educational purposes.
+              FitSlug is a personal project created for educational purposes.This application can track a user's meal information, totalling up their calories through the course of the day. It also logs additional information about the user's exercise habits. This project rebuilds on the last iteration of this project, which ran on web2py. I was able to rebuild the service by separating the API and web client. This client is a single page application on React, communicating with the Rails FitSlug API to do manage logins, record updates, etc.
             </p>
           </div>
         </div>
